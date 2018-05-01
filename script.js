b3ae2e12a8702fe93a3eafed5d2e41f087b7dcac
Damian <damian.krzem@gmail.com>
2018-05-01 07:56:50 +0200

    up

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#but").on("click", function() {
-        $.getJSON ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
+        $.getJSON ("http://api.nbp.pl/api/exchangerates/tables/A/?format=json", function(data){
             var now = new Date(Date.now());
             var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
             $("#kurs").text((data.rates[0].mid) + " " + "godzina" + " "+ formatted);      
