731df6af9d7b596f756268425e22a9ab01307f32
Damian <damian.krzem@gmail.com>
2018-05-02 15:16:10 +0200

    up

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 var currencyList = [];
 
 $(document).ready(function() {
-     getCurrencies();
-     creatOptions();
+  getCurrencies();
+  createOptions();
 });
 
-function getCurrencies(){
-$.getJSON("http://api.nbp.pl/api/exchangerates/tables/a/?format=json", function(response) {
-        $.each(response[0].rates, function(i, item) {
+function getCurrencies() {
+  $.getJSON(
+    "http://api.nbp.pl/api/exchangerates/tables/a/?format=json",
+    function(response) {
+      $.each(response[0].rates, function(i, item) {
         currencyList.push(item);
-        });
-    });
-}
-function creatOptions(){
-    var option = "";
-    for (var i = 0; i<currencyList.length; i++){
-   option += "<option value='" + currencyList[i].code + "'>" + currencyList[i].currency + "</option>";
+      });
     }
-    $("currency-list").append(option);
+  );
+}
+
+function createOptions() {
+    $.each(currencyList, function (i, item) {
+        $('currencyList').append($('<option>', { 
+            value: item.currencyList[i].currency,
+            text : item.currencyList[i].code
+        }));
+    });
 }
