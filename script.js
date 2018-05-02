031cdfeeb8d4d2a4d2e677a121eef373e6d2260d
Damian <damian.krzem@gmail.com>
2018-05-02 14:50:13 +0200

    up

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+var currencyList = [];
 
 $(document).ready(function() {
      getCurrencies();
+     creatOptions();
 });
 
 function getCurrencies(){
 $.getJSON("http://api.nbp.pl/api/exchangerates/tables/a/?format=json", function(response) {
-        $.each(response.rates[0], function(i, item) {
-          $(".currency-list").append("<option value='" + item.code + "'>" + item.currency + "</option>")
-            console.log(item.rates[0]);
+        $.each(response[0].rates, function(i, item) {
+        currencyList.push(item);
         });
     });
+}
+function creatOptions(){
+    var option = "";
+    for (var i = 0; i<currencyList.length; i++){
+   option += "<option value='" + currencyList[i].code + "'>" + currencyList[i].currency + "</option>";
+    }
+    $("currency-list").append(option);
 }
