2e90c3503d417cac0850a756689b5b906899b736
Damian <damian.krzem@gmail.com>
2018-05-06 10:03:57 +0200

    up

--- script.js
+++ script.js
@@ -4,24 +4,45 @@ $(document).ready(function() {
   getCurrencies();
 
   $("#currencyList1").change(function(){
-        getMidFrom();
+    getMidFrom();
   });
 
   $("#currencyList2").keyup(function(e){  
-      getMidTo();     
+    setTimeout(validateId, 1000);     
 });
 
   $("#amount").keyup(function(e){  
-      calculateResult();     
+    setTimeout(validateAmount, 1000);
   });
 });
+
+function validateAmount(){
+  var pattern = /^(\d{1,6})+((\.|\,)\d{1,2})?$/;
+  var amo = $("#amount").val();
+    if (amo.match(pattern)){
+      $("#wrongValue").css("display", "none");
+    calculateResult(); 
+    }else{
+      $("#wrongValue").css("display", "block");
+    }
+}
+
+function validateId(){
+  var chosenId = $("#currencyList2").val();
+    if (jQuery.inArray(chosenId, currencyList) !==-1){
+    $("#wrongId").css("display", "none");
+    getMidTo();
+  }else{
+    $("#wrongId").css("display", "block");
+  }
+}
 
 function getCurrencies() {
   $.getJSON(
     "http://api.nbp.pl/api/exchangerates/tables/a/?format=json",
     function(response) {
       $.each(response[0].rates, function(i, item) {
-        currencyList.push(item);
+        currencyList.push(item.code);
          });    
         }
     ).done(function(){
@@ -32,7 +53,7 @@ function getCurrencies() {
 function createOptions() {
   var option = "";
   for (var i = 0; i < currencyList.length; i++) {
-    option += "<option value='" + currencyList[i].code + "'>" + currencyList[i].code + "</option>";
+    option += "<option value='" + currencyList[i] + "'>" + currencyList[i] + "</option>";
   }
     $("#currencyList1").append(option)
   }
@@ -59,17 +80,17 @@ function SendRequest() {
     var res = Math.round((key*currencyFromList/currencyToList)*100)/100;
     $("#result").val(res);
 }
-
+/*
 var Timer2;
 function getMidTo() {
     $("#currencyList2").keyup(function () {
         clearTimeout(Timer2);
         Timer2 = setTimeout(SendRequest2, 1000);
     });
-}
-
-function SendRequest2() {
-    var chosenCurrency2 = $("#currencyList2").val();    
+}function SendRequest2() {
+  */
+ function getMidTo(){
+  var chosenCurrency2 = $("#currencyList2").val();    
     $.getJSON("http://api.nbp.pl/api/exchangerates/rates/a/" + chosenCurrency2 + "/?format=json", function(currencyData) {   
    currencyToList = currencyData.rates[0].mid;
 });
